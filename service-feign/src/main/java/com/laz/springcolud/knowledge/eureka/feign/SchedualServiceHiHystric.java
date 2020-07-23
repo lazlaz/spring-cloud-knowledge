a57a618f7da9d3278755439754b1976ebce8d54d
zhouxingting <zxtzxt140420>
2020-07-23 22:29:05 +0800

    增加Hystrix

---
+++ service-feign/src/main/java/com/laz/springcolud/knowledge/eureka/feign/SchedualServiceHiHystric.java
@@ -0,0 +1,11 @@
+package com.laz.springcolud.knowledge.eureka.feign;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+	@Override
+	public String sayHiFromClientOne(String name) {
+		return "sorry " + name;
+	}
+}
